7dc8d8c2871818cdcbc909d033b79be5e29bfe96
Asterisk <kuro5858tube@gmail.com>
2017-05-25 21:56:36 +0900

    #3 Modified Variable.kt - 変数宣言の例を追加。
    #3 Modified Variable.kt - 変数宣言の例を追加。

--- Variable/Variable.kt
+++ Variable/Variable.kt
@@ -1,7 +1,19 @@
 // Kotlin ・・・型を変数の後ろに記載する。
 var s: String = "hoge"
-val t: String = "hogehoge"
 
 // Java
+val t: String = "hogehoge"
+
 String       s = "hoge";
 final String t = "hogehoge";
+
+// Kotlin - プリミティブな型も同じ。
+var a: Int     = 0
+// Java
+val b: Boolean = true
+
+int           a = 0;
+final boolean b = true;
+
+val    s = a.toString()
+String s = Integer.toString( a );
