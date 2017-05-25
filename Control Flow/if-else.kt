8063b849de8f9618002b0907f24ce8df8d5ad54f
Asterisk <kuro5858tube@gmail.com>
2017-05-25 22:08:43 +0900

    #5 Modified if～else.kt exmaples Java type.
    #5 Modified if～else.kt exmaples Java type.

--- Control Flow/if-else.kt
+++ Control Flow/if-else.kt
@@ -1,3 +1,4 @@
+// Kotlin
 // Traditional usage 
 var max = a 
 if (a < b) max = b
@@ -18,3 +19,23 @@ val max = if (a > b) {
     print("Choose b")
     b
 }
+
+// Java
+//if
+int max = 10;
+if( max < 20 ) max = 20;
+
+// else
+int max = 10;
+if( max < 20 ){
+    max = 20;
+}
+else {
+    max = max;
+}
+
+//3 Section operator.
+int hoge = 20;
+int fuga = 40;
+int max  = ( hoge < huga ) ? println( hoge ) : println( fuga );
+    
