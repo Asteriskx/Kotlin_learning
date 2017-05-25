c6467f9dbead9159b0655e0321778078a312cbd5
Asterisk <kuro5858tube@gmail.com>
2017-05-25 21:37:50 +0900

    #2 KotlinFirstImpression .kt

---
+++ KotlinFirstImpression/KotlinFirstImpression.kt
@@ -0,0 +1,17 @@
+///<summary>
+/// Kotlin - main 関数 
+///</summary>
+fun main(args:Array<String>) {
+    println("Hello, world.")
+}
+
+// 以下、Java との比較
+
+///<summary>
+/// Java - main 関数 
+///</summary>
+public class Main {
+    public static final int main(String[] args) {
+        System.out.println("Hello, world.");
+    }
+}
