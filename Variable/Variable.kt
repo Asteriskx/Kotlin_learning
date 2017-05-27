ad83b037c60ce73d79bb9ae55e65f36c240ea801
Asterisk <kuro5858tube@gmail.com>
2017-05-27 22:16:58 +0900

    #6 Modified Variable.kt コメント整理。
    #6 Modified Variable.kt コメント整理。

--- Variable/Variable.kt
+++ Variable/Variable.kt
@@ -1,19 +1,21 @@
 // Kotlin ・・・型を変数の後ろに記載する。
-var s: String = "hoge"
-
-// Java
+var s: String = "hoge
 val t: String = "hogehoge"
 
+// Java
 String       s = "hoge";
 final String t = "hogehoge";
 
 // Kotlin - プリミティブな型も同じ。
 var a: Int     = 0
-// Java
 val b: Boolean = true
 
+// Java
 int           a = 0;
 final boolean b = true;
 
+// Kotlin
 val    s = a.toString()
+
+// Java
 String s = Integer.toString( a );
